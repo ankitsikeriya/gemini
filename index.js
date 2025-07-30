cd33dc38ac943257b81ad39eec0183007cd1a989
Ankit sikeriya <156462765+ankitsikeriya@users.noreply.github.com>
2025-07-30 23:43:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const platform = os.platform();
 //
 dotenv.config(); // Load environment variables from .env file
 //history initialize
-const History = []; 
+const History = [];  
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const execPromise = promisify(exec);
