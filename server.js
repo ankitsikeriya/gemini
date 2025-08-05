4c8d90a62c99ca91f502e5e45e380cdd1c3d934f
Ankit sikeriya <156462765+ankitsikeriya@users.noreply.github.com>
2025-08-05 23:24:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file   
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
    
-async function main() {  
+async function main() {   
   const response = await ai.models.generateContent({ 
     model: "gemini-2.0-flash",
     contents: "write a html and css code to give you a prompt and display the response",
