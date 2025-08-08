3cb1d1f4f9c9bedb695768d264286b18e9a4b803
Ankit sikeriya <156462765+ankitsikeriya@users.noreply.github.com>
2025-08-08 23:54:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
         
 async function main() {   
   const response = await ai.models.generateContent({    
-    model: "gemini-2.0-flash",
+    model: "gemini-2.0-flash", 
     contents: "write a html and css code to give you a prompt and display the response",  
   });
   console.log(response.text);
